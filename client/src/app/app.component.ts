1fb132c0ce3046be78f5fa74e88acc05f1ca60b7
dphasst17 <phathuu12@gmail.com>
2024-01-20 15:38:08 +0700

    update project pages

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,13 +3,8 @@ import { CommonModule } from '@angular/common';
 import { Router,RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
-<<<<<<< Updated upstream
 import { Global } from './service/global.service';
 import {ApiService} from "../app/api/api.service"
-=======
-
-
->>>>>>> Stashed changes
 @Component({
   selector: 'app-root',
   standalone: true,
--- client/src/app/pages/product/product.component.ts
+++ client/src/app/pages/product/product.component.ts
@@ -2,27 +2,15 @@ import { Component } from '@angular/core';
 import { LeaderProjectComponent } from '../../components/project/leader-project/leader-project.component';
 import { StaffProjectComponent } from '../../components/project/staff-project/staff-project.component';
 import { CommonModule } from '@angular/common';
-<<<<<<< Updated upstream
-@Component({
-  selector: 'app-product',
-  standalone: true,
-  imports: [LeaderProjectComponent,StaffProjectComponent,CommonModule],
-=======
-import { StaffProjectComponent1 } from '../../components/project/staff-project1/staff-project1.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [LeaderProjectComponent,StaffProjectComponent1, StaffProjectComponent, CommonModule, ReactiveFormsModule],
->>>>>>> Stashed changes
+  imports: [LeaderProjectComponent, StaffProjectComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-<<<<<<< Updated upstream
   role = localStorage.getItem('role')
-=======
-    role = localStorage.getItem('role') 
->>>>>>> Stashed changes
 }
