f7af0ad487eb0eaf0a972520c60049f92e1eaa28
dphasst17 <phathuu12@gmail.com>
2024-02-03 15:50:03 +0700

    change leader and staff project components

--- client/src/app/components/project/staff-project/staff-project.component.ts
+++ client/src/app/components/project/staff-project/staff-project.component.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { Global } from '../../../service/global.service';
+import { ViewProjectComponent } from '../../form/view-project/view-project.component';
+import { PmViewTaskComponent } from '../../form/view-task/pm-view-task/pm-view-task.component';
+import { StaffViewTaskComponent } from '../../form/view-task/staff-view-task/staff-view-task.component';
 @Component({
   selector: 'app-staff-project',
-  standalone: true, // Bỏ qua thuộc tính này
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  standalone: true,
+  imports: [CommonModule,ViewProjectComponent,PmViewTaskComponent,StaffViewTaskComponent],
   templateUrl: './staff-project.component.html',
-  styleUrls: ['./staff-project.component.css'] // Sửa thành styleUrls
+  styleUrls: ['./staff-project.component.css']
 })
-export class StaffProjectComponent {
-
-
-  constructor() {
+export class StaffProjectComponent implements OnInit {
+  position:string = ''
+  constructor(private global:Global) {
   }
   ngOnInit() {
+    this.global.currentInfo.subscribe(i => this.position = i[0]?.position)
   }
   onSubmit = (input:{taskname:string, description: string, project: string, assignedTo: string, priority: string, status: string}) => {
     document.getElementById('taskname')?.classList.add(input.taskname === "" ? 'border-red-500' : 'border-green-500')
