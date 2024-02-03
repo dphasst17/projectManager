f809fbe0784b26a759602ba0ffe47dd7e132ec8c
dphasst17 <phathuu12@gmail.com>
2024-02-03 14:53:05 +0700

    update:Add multi global variable in global.service.ts

--- client/src/app/service/global.service.ts
+++ client/src/app/service/global.service.ts
@@ -5,26 +5,44 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class Global {
-  public project:any // Biến toàn cục của bạn
   public isLogin:boolean;
   public token:string;
   public role:string
-  public exp:number
+  public exp:number;
+  public modalDetail:boolean = false;
+  private project = new BehaviorSubject<any[]>([]);
+  currentProject = this.project.asObservable();
   private infoSource = new BehaviorSubject<any[]>([]);
   currentInfo = this.infoSource.asObservable();
   private staffSource = new BehaviorSubject<any[]>([]);
   currentStaff = this.staffSource.asObservable();
   constructor() {
-    this.project = null;
     this.role = localStorage.getItem('role') || ''
     this.isLogin = localStorage.getItem('isLogin') === 'true'
     this.token = localStorage.getItem('t') || ''
     this.exp = Number(localStorage.getItem('e')) || 0
+  }
+  changeProject(project:any[]){
+    this.project.next(project);
   }
   changeInfo(info: any[]) {
     this.infoSource.next(info);
   }
   changeStaffData(data:any[]) {
     this.staffSource.next(data);
+  }
+  getValueLocal(key:string){
+    return localStorage.getItem(key)
+  }
+  pagination(length:number,itemsInPage:number) {
+    let arrPage:any = []
+    const handleFirst = length % itemsInPage === 0 ? length / itemsInPage : (length / itemsInPage) + 1
+    for(let i = 1; i <= handleFirst; i++){
+      arrPage = [...arrPage, i]
+    }
+    return arrPage
+  }
+  calculatorPage(length:number,activePages:number){
+    return length * activePages
   }
 }
