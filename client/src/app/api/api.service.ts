5d04fff7eac844bb7959c0b6bffc7d0e48cfed0a
dphasst17 <phathuu12@gmail.com>
2024-02-03 14:50:05 +0700

    add multi fetch api in api.service

--- client/src/app/api/api.service.ts
+++ client/src/app/api/api.service.ts
@@ -4,12 +4,27 @@ import { environment } from '../../environment/env';
   providedIn: 'root'
 })
 export class ApiService {
-  fetchProject = async() => {
-    return fetch(`${environment.apiUrl}`).then(res => res.json());
+  /* All fetch project */
+
+  fetchCreateUser = async(data:any,token:string) => {
+    return fetch(`${environment.apiUrl}/auth/register`,{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json',
+        'Authorization':`Bearer ${token}`
+      },
+      body:JSON.stringify(data)
+    }).then(res => res.json());
   }
+
+  fetchProjectById = async(id:any) => {
+    return fetch(`${environment.apiUrl}/api/project/id/${id}`).then(res => res.json());
+  }
+
   fetchExpense = async() => {
     return fetch(`${environment.apiUrl}/api/project/expense`).then(res => res.json())
   }
+
   fetchAutoUpdateStatus = async() => {
     return fetch(`${environment.apiUrl}/api/project/update/status/auto`,{
       method:'POST',
@@ -18,13 +33,49 @@ export class ApiService {
       }
     }).then(res => res.json())
   }
+
+  fetchProject = async() => {
+    return fetch(`${environment.apiUrl}/api/project`).then(res => res.json());
+  }
+
+  fetchProjectByStaff = async(token:string) => {
+    return fetch(`${environment.apiUrl}/api/project/staff`,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization':`Bearer ${token}`
+      }
+    }).then(res => res.json());
+  }
+
   fetchTaskProcess = async(status:string) => {
     return fetch(`${environment.apiUrl}/api/project/task/status/${status}`).then(res => res.json())
   }
+
   fetchProjectByStatus = async(status:string) => {
     return fetch(`${environment.apiUrl}/api/project/status/${status}`).then(res => res.json())
   }
-  
+
+  fetchTaskTodo = async(token:string) => {
+    return fetch(`${environment.apiUrl}/api/project/task/todo`,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization':`Bearer ${token}`
+      }
+    }).then(res => res.json())
+  }
+  fetchTaskTodoByStaff = async(token:string) => {
+    return fetch(`${environment.apiUrl}/api/project/task/todo/staff`,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization':`Bearer ${token}`
+      }
+    }).then(res => res.json())
+  }
+  /* All fetch auth */
+
   fetchLogin = async(data:any) => {
     return fetch(`${environment.apiUrl}/auth/login`,{
       method:'POST',
@@ -34,6 +85,19 @@ export class ApiService {
       body:JSON.stringify(data)
     }).then(res => res.json())
   }
+
+  fetchForgotPassword = async(data:any) => {
+    return fetch(`${environment.apiUrl}/auth/forgot`,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    }).then(res => res.json())
+  }
+
+  /* All fetch user */
+
   fetchInfo = async(token:any) => {
     return fetch(`${environment.apiUrl}/user/info`,{
       method:'POST',
@@ -45,5 +109,33 @@ export class ApiService {
   }
   fetchStaff = async() => {
     return fetch(`${environment.apiUrl}/user/staff`).then(res => res.json())
+  }
+  fetchUpdateAction = async(token:string,data:any) => {
+    return fetch(`${environment.apiUrl}/user/update/action`,{
+      method:'PUT',
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization':`Bearer ${token}`
+      },
+      body:JSON.stringify(data)
+    }).then(res => res.json())
+  }
+  fetchCreateProject = async (data:any) => {
+    return fetch(`${environment.apiUrl}/api/project/create`,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    }).then(res => res.json())
+  }
+  fetchCreateTask = async (idProject:any,detail:any[]) => {
+    return fetch(`${environment.apiUrl}/api/project/create/task`,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({idProject,detail})
+    }).then(res => res.json())
   }
 }
