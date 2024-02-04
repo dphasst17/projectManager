e52523541ab67dafa62b1b116f24b9686d691b48
dphasst17 <phathuu12@gmail.com>
2024-02-04 18:01:20 +0700

    update route user

--- server/api/user/user.js
+++ server/api/user/user.js
@@ -3,10 +3,19 @@ import * as sqlQuery from "../../db/statement/user.js"
 import { poolConnectDB } from "../../db/db.js";
 import * as response from "../../utils/handle.js"
 import * as message from "../../utils/message.js"
-import {verify} from "../../middleware/index.js"
+import {handleRoleAdmin, verify} from "../../middleware/index.js"
 const router = express.Router();
 const pool = poolConnectDB()
-router.post('/info/',verify,(req,res) => {
+router.post('/insert',verify,handleRoleAdmin,(req,res) => {
+    const data = req.body;
+    const sql = sqlQuery.insertUser(data);
+    pool.query(sql,(err,results) => {
+        response.errResponseMessage(res,err,500,message.err500Message())
+
+        response.successResponseMessage()
+    })
+})
+router.post('/info',verify,(req,res) => {
     const idUser = req.idUser
     const sql = sqlQuery.getUser(idUser)
     pool.query(sql,(err,results) => {
@@ -31,12 +40,27 @@ router.put('/update',verify,(req,res) => {
         response.successResponseMessage(res,200,message.updateItemsMessage('user'))
     })
 })
+router.put('/update/action',verify,handleRoleAdmin,(req,res) => {
+    const data = req.body;
+    const sql = sqlQuery.updateAction(data.idUser,data.action);
+    pool.query(sql,(err,results) => {
+        response.errResponseMessage(res,err,message.err500Message())
+
+        response.successResponseMessage(res,200,message.updateItemsMessage(`action for ${data.idUser}`))
+    })
+})
 router.get('/staff',(req,res) => {
-    const sql = sqlQuery.getAll()
+    const sql = sqlQuery.getStaff()
     pool.query(sql,(err,results) => {
         response.errResponseMessage(res,err,500,message.err500Message())
 
         response.successResponseData(res,200,results)
+    })
+})
+router.post('/all',handleRoleAdmin,(req,res) => {
+    const sql = sqlQuery.getStaff();
+    pool.query(sql,(err,results) => {
+        response.errResponseMessage(res,err,message.err500Message())
     })
 })
 export default router
