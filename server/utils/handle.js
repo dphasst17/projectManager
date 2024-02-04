24de2394e1b5c5798107c8a5585af0db8c65add5
dphasst17 <phathuu12@gmail.com>
2024-02-04 18:04:55 +0700

    update custom response in utils/handle

--- server/utils/handle.js
+++ server/utils/handle.js
@@ -19,6 +19,9 @@ export const successResponseMessage = (res, status, message) => {
 export const successResponseData = (res, status, data) => {
   res.status(status).json({ status: status, data: data })
 }
+export const successResponseMessageAndData = (res, status,message, data) => {
+  res.status(status).json({ status: status, message:message, data: data })
+}
 export const formatDate = (date) => {
   return new Date(date).toLocaleDateString('vi-VN', { day: '2-digit', month: '2-digit', year: 'numeric' })
 }
@@ -65,7 +68,7 @@ export const handleSendMail = (res, data) => {
         html: `<h3>${content}</h3>` // Nội dung email
       }
       await transport.sendMail(mailOptions)
-      res.status(200).json({ message: 'Email sent successfully.' })
+      res.status(200).json({status:200, message: 'Email sent successfully.' })
     } catch (error) {
       console.log(error)
       res.status(500).json({ errors: error.message })
