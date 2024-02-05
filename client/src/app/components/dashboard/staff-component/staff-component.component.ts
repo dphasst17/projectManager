a981c93f8700c38700b64715e745d785a6730514
dphasst17 <phathuu12@gmail.com>
2024-02-05 14:18:45 +0700

    feature: add handle for edit info user and change password

--- client/src/app/components/dashboard/staff-component/staff-component.component.ts
+++ client/src/app/components/dashboard/staff-component/staff-component.component.ts
@@ -3,21 +3,41 @@ import { Component, OnInit } from '@angular/core';
 import { Global } from '../../../service/global.service';
 import { ApiService } from '../../../api/api.service';
 import { ViewProjectComponent } from '../../form/view-project/view-project.component';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-staff-component',
   standalone: true,
-  imports: [CommonModule,ViewProjectComponent],
+  imports: [CommonModule,ViewProjectComponent,ReactiveFormsModule],
   templateUrl: './staff-component.component.html',
   styleUrl: './staff-component.component.css'
 })
 export class StaffComponentComponent implements OnInit {
+  formEdit: FormGroup;
+  formPassword: FormGroup;
   dataInfo:any = [];
   taskByProject:any = [];
   staffProject:any = [];
   projectDetail:any = [];
   listStaffProject:any = [];
   resultProject:any = [];
-  constructor(private global: Global, private apiService: ApiService){}
+  formBoolean:any = {
+    edit:false,
+    password: false
+  }
+  constructor(private global: Global, private apiService: ApiService){
+    this.formPassword = new FormGroup({
+      'current': new FormControl(null, Validators.required),
+      'new': new FormControl(null, Validators.required),
+      'confirm': new FormControl(null, Validators.required),
+    });
+    this.formEdit = new FormGroup({
+      'name': new FormControl(this.dataInfo?.name, Validators.required),
+      'phone': new FormControl(this.dataInfo?.phone, Validators.required),
+      'email': new FormControl(this.dataInfo?.email, [Validators.required, Validators.email]),
+      'address': new FormControl(this.dataInfo?.address, Validators.required),
+    });
+    
+  }
   ngOnInit(){
     const token= this.global.getValueLocal('t')
     if(token){
@@ -29,6 +49,12 @@ export class StaffComponentComponent implements OnInit {
     }
     this.global.currentInfo.subscribe(staff => {
       this.dataInfo = staff[0]
+      this.formEdit.patchValue({
+        'name': this.dataInfo?.name,
+        'phone': this.dataInfo?.phone,
+        'email': this.dataInfo?.email,
+        'address': this.dataInfo?.address,
+      });
       const listProject = Array.from(new Set(staff[0]?.task.map((e:any) => e.nameProject)))
       const formatProject = listProject.map((e:any) => {
         return {
@@ -39,22 +65,53 @@ export class StaffComponentComponent implements OnInit {
       this.resultProject = formatProject
     })
   }
-  showModal: boolean = false;
-  detailModel:boolean = false
-  modalScale: string = 'scale-0'
-  changeModal(){
-    this.showModal = !this.showModal
-    this.modalScale = this.showModal === true ? 'scale-100' : 'scale-0'
+  onSubmit(){
+    if (this.formEdit.invalid) {
+      Object.keys(this.formEdit.controls).forEach(field => {
+        const control = this.formEdit.get(field);
+        control?.markAsTouched({ onlySelf: true });
+      });
+      return
+    }
+    this.apiService.fetchUpdateUser(this.global.token,this.formEdit.value)
+    .then(res => {
+      alert(res.message)
+      if(res.status === 200){
+        this.effectModal('edit')
+        this.dataInfo = this.formEdit.value
+      }
+    })
+    console.log(this.formEdit.value)
   }
-  viewDetail(){
-    this.detailModel = !this.detailModel
+  onSubmitPassword(){
+    const value = this.formPassword.value
+    if (this.formPassword.invalid) {
+      Object.keys(this.formPassword.controls).forEach(field => {
+        const control = this.formPassword.get(field);
+        control?.markAsTouched({ onlySelf: true });
+      });
+      return
+    }
+    if(value.confirm !== value.new){
+      alert("Confirm password doesn't match with new password")
+      return
+    }
+    this.apiService.fetchUpdatePassword(this.global.token,{current:value.current,new:value.new})
+    .then(res => {
+      alert(res.message)
+      if(res.status === 200){
+        this.effectModal('password')
+      } 
+    })
+  }
+  effectModal(modal:string){
+    this.formBoolean[modal] = !this.formBoolean[modal]
   }
   fetchData(idProject:any){
     this.apiService.fetchProjectById(idProject).then(res => {
       if(res.status === 200){
         this.projectDetail = res.data
         this.listStaffProject = res.data[0].teamDetail.flatMap((e:any) => e.staff)
-        /* console.log(res.data[0].teamDetail.flatMap((e:any) => e.staff)) */
       }
     })
   }
